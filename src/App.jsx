569a869e134b00f991c1cfb9f101e9670b0aed4f
Marvin Gomez <mogomez@ucsc.edu>
2024-06-19 22:55:48 -0700

    Todo list generates properly

--- src/App.jsx
+++ src/App.jsx
@@ -2,21 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-
-var todoList = [
-  {
-    id: 1,
-    title: "Kilter Board"
-  },
-  {
-    id: 2,
-    title: "Muay Thai"
-  },
-  {
-    id: 3,
-    title: "Kickboxing"
-  }
-]
+import TodoList from './TodoList'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -24,6 +10,7 @@ function App() {
   return (
     <main>
       <h1>Todo List</h1>
+      <TodoList />
     </main>
   )
 }
--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -1,3 +1,23 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+var todoList = [
+    {
+      id: 1,
+      title: "Kilter Board"
+    },
+    {
+      id: 2,
+      title: "Muay Thai"
+    },
+    {
+      id: 3,
+      title: "Kickboxing"
+    }
+]
+
 function TodoList () {
     return (
       <ul>
