824470e7755859cb2f0e960cf7170d04d52902c2
Marvin Gomez <mogomez@ucsc.edu>
2024-07-16 00:22:16 -0700

    step 2 done

--- src/AddTodoForm.jsx
+++ src/AddTodoForm.jsx
@@ -6,10 +6,13 @@ import './App.css'
 function AddTodoForm (props) {
     const [todoTitle, setTodoTitle] = useState('');
 
+    const handleTitleChange = (event) => {
+        const newTodoTitle = event.target.value;
+        setTodoTitle(newTodoTitle);
+    }
+
     const handleAddTodo = (event) => {
         event.preventDefault();
-        const todoTitle = event.target.elements.title.value;
-        console.log(todoTitle);
         props.onAddTodo(todoTitle);
         setTodoTitle('');
     };
@@ -17,7 +20,13 @@ function AddTodoForm (props) {
     return (
         <form onSubmit={handleAddTodo}>
             <label htmlFor="todoTitle">Title </label>            
-            <input name="title" type="text" id="todoTitle" value={todoTitle} onChange={(e)=>setTodoTitle(e.target.value)}/>
+            <input 
+                name="title" 
+                type="text" 
+                id="todoTitle" 
+                value={todoTitle} 
+                onChange={handleTitleChange}
+            />
             <button type="submit">Add</button>
         </form>
     );
